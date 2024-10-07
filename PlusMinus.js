2a9c76b4cce02b268feaedca0f30551ce6f639e0
frank <imbruglia100@gmail.com>
2024-10-07 09:26:05 -0400

    PlusMinus js

---
+++ PlusMinus.js
@@ -0,0 +1,51 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    let positive = 0
+    let negative = 0
+    let neutral = 0
+
+    arr.forEach(el => {
+        el > 0 ? positive++ :
+        el === 0 ? neutral++ :
+        negative++
+    });
+
+    console.log((positive/arr.length).toFixed(6))
+    console.log((negative/arr.length).toFixed(6))
+    console.log((neutral/arr.length).toFixed(6))
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
