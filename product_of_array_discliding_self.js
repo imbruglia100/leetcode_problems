9e63e35a8b679161ea3bdf5db6c6d89b35a041fe
frank <imbruglia100@gmail.com>
2024-11-13 16:47:55 -0500

    product of array

---
+++ product_of_array_discliding_self.js
@@ -0,0 +1,27 @@
+// Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except nums[i].
+
+//medium
+
+const productExceptSelf = nums => {
+    const n = nums.length;
+        const pref = [];
+        const post = [];
+        const res = [];
+
+        pref[0] = 1;
+        post[n - 1] = 1;
+        
+        for (let i = 1; i < n; i++) {
+            pref[i] = nums[i - 1] * pref[i - 1];
+        }
+        for (let i = n - 2; i >= 0; i--) {
+            post[i] = nums[i + 1] * post[i + 1];
+        }
+        for (let i = 0; i < n; i++) {
+            res[i] = pref[i] * post[i];
+        }
+        return res;
+}
+
+
+console.log(productExceptSelf([1,2,4,6]))
